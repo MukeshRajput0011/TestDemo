fe50aadecb11e8d2f8447fbebed28465aa5854cd
MukeshRajput0011 <tomukeshrajput@gmail.com>
2020-12-24 14:26:41 +0530

    Test Repo

---
+++ js/MyScript.js
@@ -0,0 +1,8 @@
+
+		function TextChanged()
+		{
+		     document.getElementById("p1").innerHTML = "Text has changed!";
+		     document.getElementById("p2").style.fontSize = "35px";
+		     document.getElementById("p2").style.color = "red";
+		     document.getElementById("p1").style.backgroundColor = "pink";
+		}
